973b9742554484b6ea336804394a6b41f7fd9402
LaurentCorten <padbol1030@gmail.com>
2026-09-09 16:04:13 +0200

    end of day

---
+++ Assets/#Project/Scripts/ChargeAction.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Charge", story: "[Agent] charges toward [Target]", category: "Action/Boss Fight", id: "5d253cc5643baf9ed428640e52f90a52")]
+public partial class ChargeAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
